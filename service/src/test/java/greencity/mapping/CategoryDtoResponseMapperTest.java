086fc0a16714dc11ef7e452ccb0076e200860a3b
Maryna <m51571750@gmail.com>
2024-03-01 11:59:50 +0300

    Created unit test to  CategoryDtoResponseMapper

---
+++ service/src/test/java/greencity/mapping/CategoryDtoResponseMapperTest.java
@@ -0,0 +1,63 @@
+package greencity.mapping;
+
+import greencity.dto.category.CategoryDtoResponse;
+import org.junit.jupiter.api.Test;
+import greencity.entity.Category;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.modelmapper.ModelMapper;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+@ExtendWith(SpringExtension.class)
+class CategoryDtoResponseMapperTest {
+    private final ModelMapper modelMapper = new ModelMapper();
+    private final CategoryDtoResponseMapper categoryDtoResponseMapper = new CategoryDtoResponseMapper();
+    @Test
+    void convert_CategoryDtoResponseMapperTest_ShouldMapCorrectly() {
+        Category category = new Category(55L,"Name", "Ім'я", null, null);
+
+        CategoryDtoResponse expected = CategoryDtoResponse.builder()
+                .id(category.getId())
+                .name(category.getName())
+                .build();
+
+        CategoryDtoResponse actual = categoryDtoResponseMapper.convert(category);
+
+        assertNotNull(actual);
+        assertEquals(category.getId(), actual.getId());
+        assertEquals(category.getName(), actual.getName());
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void convert_CategoryDtoResponseMapperTest_ShouldMapToCategoryWithNullNameAndNullId() {
+        Category category = new Category();
+
+        CategoryDtoResponse actual = categoryDtoResponseMapper.convert(category);
+
+        assertNotNull(actual);
+        assertEquals(null, actual.getId());
+        assertEquals(null, actual.getName());
+    }
+
+    @Test
+    void convert_CategoryDtoResponseMapperTest_ShouldReturnNullPointerException() {
+        Category category = null;
+
+        assertThrows(NullPointerException.class, () -> {
+            categoryDtoResponseMapper.convert(category);
+        });
+    }
+
+    @Test
+    void convert_CategoryDtoResponseMapperTest_ShouldProduceSameResult() {
+        Category category = new Category();
+        category.setId(77L);
+        category.setName("Some Name");
+
+        CategoryDtoResponse actual = categoryDtoResponseMapper.convert(category);
+        CategoryDtoResponse categoryByModelMapper = modelMapper.map(category, CategoryDtoResponse.class);
+
+        assertEquals(categoryByModelMapper, actual);
+    }
+}
